c33102a1699958c1b54bd00661a8aa8c63c6ce84
Ranjeet Kumar Yadav <113177622+Ranjeet-Kr-Yadav@users.noreply.github.com>
2023-07-29 18:48:39 +0000

    Datatype

---
+++ Practice/02_datatype.js
@@ -0,0 +1,19 @@
+"use strict"; //all JS code as newer version
+
+// alert()  we are using nodejs, not browser 
+
+// DATATYPE
+let name = "Ranjeet";
+let age = 21;
+
+console.table([name,age]);
+
+// some datatype are
+
+// number => 2 to power 53
+// BigInt
+// string
+// undefined is undefined
+// null is a object
+// boolean => true false
+// symbol =>unique
