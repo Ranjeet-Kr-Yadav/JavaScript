23cdabf37bd02121b96076ae0a20859d113ea820
Ranjeet Kumar Yadav <113177622+Ranjeet-Kr-Yadav@users.noreply.github.com>
2023-07-29 18:22:48 +0000

    container config done

---
+++ Practice/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 123456; //Not changeable
+let accountEmail = "ranjeetyadav7536@gmail.com"; //value changeable
+var accountPassword = 123456; // value changeable and also redefine variable
+
+
+// print multiple variable at a same time 
+console.table([accountId,accountEmail,accountPassword])
