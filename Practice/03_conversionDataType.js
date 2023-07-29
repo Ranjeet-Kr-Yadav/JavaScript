0f9fab19cda8afad27fc1ac9a4598a65b70bc97e
Ranjeet Kumar Yadav <113177622+Ranjeet-Kr-Yadav@users.noreply.github.com>
2023-07-29 18:57:58 +0000

    conversion

---
+++ Practice/03_conversionDataType.js
@@ -0,0 +1,12 @@
+// let score = 33;  number
+// let score = null;    object
+// let score = "six"; string
+let score = undefined //undefined
+
+// boolean = true(1), false(0)
+
+
+
+
+// typeof show the type of data
+console.log(typeof score);
