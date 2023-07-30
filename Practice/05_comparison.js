6a4277de2ce796b46779428e4f611992fec0df09
Ranjeet Kumar Yadav <ranjeetyadav7536@gmail.com>
2023-07-30 18:20:11 +0000

    comparison

---
+++ Practice/05_comparison.js
@@ -0,0 +1,10 @@
+console.log("2">1);
+console.log("02">1);
+console.log(2>1);
+console.log(null>1);
+console.log(undefined>1);
+
+// == it check only value
+console.log(2==2)
+console.log("2"==2)
+// === it check strictly data type and value
