1bc07110a06f7873029545161884a44bc6a7735a
Ranjeet Kumar Yadav <ranjeetyadav7536@gmail.com>
2023-07-30 19:45:00 +0000

    string

---
+++ Practice/08_string.js
@@ -0,0 +1,15 @@
+const name = "ranjeet";
+const lastName = "yadav";
+
+// console.log(name + lastName);
+
+console.log(`my name is ${name + lastName}`)
+
+const newName = new String("Ranjeet kunar")
+
+console.log(newName.length)
+console.log(newName.toUpperCase())
+console.log(newName.charAt(2))
+console.log(newName.indexOf(`t`))
+console.log(newName.trim()) // extra space remove from start and end
+console.log(newName.slice(2, 8))
