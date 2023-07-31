ace97acf5d4230703e1ba0e959d340dce4c18da9
Ranjeet Kumar Yadav <ranjeetyadav7536@gmail.com>
2023-07-31 22:05:53 +0000

    Date & Time

---
+++ Practice/09_date_time.js
@@ -0,0 +1,10 @@
+let myDate = new Date();
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+
+let myTime =Date.now()
+
+console.log(myTime)
