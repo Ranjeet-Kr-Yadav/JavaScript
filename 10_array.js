7da51524bc96d4884034d54dcbd3ffe19060706f
Ranjeet Kumar Yadav <ranjeetyadav7536@gmail.com>
2023-08-09 18:42:52 +0000

    Array

---
+++ 10_array.js
@@ -0,0 +1,57 @@
+// Array 
+
+const arr =[0,1,2,3,4,5,6,7,8,9];
+
+// arr.pop();  //remove last element
+// arr.push(90);  //add element in last of array
+// arr.shift(); //remove element from start
+// arr.unshift(88); //add element in start of array
+
+// console.log(arr);
+// console.log(arr.indexOf(3)); // show the index of arr
+
+
+// slice splice
+// const arr1 = arr.slice(1,4); //not chanage any element in orriginal array
+// console.log( arr);
+// console.log( arr1);
+// console.log("A", arr);
+
+// const arr2 = arr.splice(1,4); //chanage  in orriginal array
+// console.log("c", arr);
+// console.log( arr2);
+// console.log("B", arr2);
+
+
+// const indian_hero = ["Ram","Krishna", "Hanuman", "Arjun" ];
+// const raj_hero = ["Nagraj","Doga", "Dhruva", "Saktiman" ];
+
+// console.log(indian_hero);
+// console.log(raj_hero);
+
+// const allHeroes = indian_hero.concat(raj_hero);
+// console.log(allHeroes);
+
+// const all_new_hero = [...indian_hero,...raj_hero] //spred value
+
+// console.log(all_new_hero)
+
+
+// const anthor_array = [1,2,3,[2,3,5,[6,7,8,[9,0]]]]
+// const real_arr = anthor_array.flat(Infinity)
+// console.log(real_arr)
+
+
+console.log(Array.isArray("ranjeet"))
+console.log(Array.from("ranjeet"))
+console.log(Array.from({name:"ranjeet"}))
+
+
+
+
+
+
+
+
+
+
