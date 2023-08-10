c235e0e4e01970144d9284cd1991b5b53f693a6b
Ranjeet Kumar Yadav <ranjeetyadav7536@gmail.com>
2023-08-10 05:56:23 +0000

    function

---
+++ Practice/12_function.js
@@ -0,0 +1,53 @@
+
+
+
+function func_name(){
+    // console.log("R");
+    // console.log("A");
+    // console.log("N");
+    // console.log("J");
+    // console.log("E");
+    // console.log("E");
+    // console.log("T");
+}
+
+// func_name();
+
+//Add number
+
+// function addNumber(num1,num2){
+//     console.log(num1 + num2)
+// }
+
+// addNumber(2,3)
+
+
+function addNumber(num1,num2){
+    // let result = num1 + num2;
+    // return result;
+
+    return num1+num2;
+}
+
+const result= addNumber(2,3)
+
+// console.log("Result", result);
+
+function login(user) {
+    return `Welcome ${user} `
+}
+// console.log(login("Ranjeet"))
+
+function calculateCardPrice(...num){ 
+    return num;
+}
+// console.log(calculateCardPrice(200,300,400))
+
+
+const userName = {
+    realusername : "Ranjeet",
+    price :199
+
+}
+
+ 
